9494572656875de81b5a934d7b252299252bf30c
Abdurehman Kero <keroabdurehman@gmail.com>
2025-12-12 09:35:30 +0300

    results page added

--- client/src/Pages/Results/Resulst.jsx
+++ client/src/Pages/Results/Resulst.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios"; 
+import classes from "./Results.module.css";
+import ProductCard from "../../Components/Product/ProductCard";
+import Layout from "../../Layout/Layout";
 
-const Resulst = () => {
+function Result() {
+  const { categoryName } = useParams();
+  const [filteredProduct, setFilteredProduct] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`https://fakestoreapi.com/products/category/${categoryName}`)
+      .then((res) => setFilteredProduct(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+  console.log(filteredProduct);
   return (
-    <div>Resulst</div>
-  )
+    <Layout>
+      <section>
+        <h1 style={{ padding: "30px" }}>Results</h1>
+        <p style={{ padding: "30px" }}>Category / {categoryName}</p>
+        <hr />
+        <div className={classes.products_container}>
+          {filteredProduct?.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      </section>
+    </Layout>
+  );
 }
 
-export default Resulst
+export default Result;
