e9f05a297fde8689953b5bd37bb116a4d44579fe
Abdurehman Kero <keroabdurehman@gmail.com>
2025-12-13 10:45:05 +0300

    loading component added

--- client/src/Components/Product/Product.jsx
+++ client/src/Components/Product/Product.jsx
@@ -2,20 +2,36 @@ import { useEffect, useState } from "react";
 import classes from "./product.module.css";
 import axios from "axios";
 import ProductCard from "./ProductCard";
+import Loader from "../../Components/Loader/Loader"
 function Product() {
   const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
+    setIsLoading(true);
     axios
       .get("https://fakestoreapi.com/products")
-      .then((res) => setProducts(res.data))
-      .catch((err) => console.log(err));
+      .then((res) => {
+        setIsLoading(false)
+        setProducts(res.data)
+      })
+      .catch((err) => { 
+        setIsLoading(false);
+        console.log(err)
+      }
+      );
   }, []);
   return (
-    <section className={classes.products_container}>
-      {products?.map((product) => (
-        <ProductCard key={product.id} product={product} />
-      ))}
-    </section>
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <section className={classes.products_container}>
+          {products?.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </section>
+      )}
+    </>
   );
 }
 
--- client/src/Pages/productDetail/ProductDetails.jsx
+++ client/src/Pages/productDetail/ProductDetails.jsx
@@ -1,31 +1,32 @@
 import { useParams } from "react-router-dom"; 
 import { useEffect, useState } from "react";
 import axios from "axios"; 
-// import Spinner from "../../components/Loading/Spinner";
+ 
 import Layout from "../../Layout/Layout";
 import ProductCard from "../../Components/Product/ProductCard";
+import  Loader from "../../Components/Loader/Loader";
 
 function ProductDetail() {
   const { productId } = useParams();
   const [product, setProduct] = useState({});
-  // const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   useEffect(() => {
-    // setIsLoading(true);
+    setIsLoading(true);
     axios
       .get(`https://fakestoreapi.com/products/${productId}`)
       .then((res) => {
+        setIsLoading(false);
         setProduct(res.data);
-        // setIsLoading(false);
       })
       .catch((err) => {
-        // setIsLoading(false);
+        setIsLoading(false);
         console.log(err);
       });
   }, []);
   console.log(product);
   return (
     <Layout>
-      { <ProductCard product={product}   />}
+      {isLoading? (<Loader/>):  <ProductCard product={product}   />}
     </Layout>
   );
 }
