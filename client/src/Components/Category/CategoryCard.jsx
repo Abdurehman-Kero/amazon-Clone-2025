61a62376a6bf6319f93353e4030ec155cbc292b5
Abdurehman Kero <keroabdurehman@gmail.com>
2025-12-17 00:45:51 +0300

    on deploment, category folder fixed

--- client/src/Components/Category/CategoryCard.jsx
+++ client/src/Components/Category/CategoryCard.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function CategoryCard({ info }) {
   return (
     <div className={classes.category}>
-      <Link to={`category/${info?.name}`}>
+      <Link to={`/category/${info?.name}`}>
         <span>
           <h2>{info?.title}</h2>
         </span>
