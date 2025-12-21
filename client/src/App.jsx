0ca266029f738031647bf51808d1acc924bc6f42
Abdurehman Kero <keroabdurehman@gmail.com>
2025-12-21 11:56:02 +0300

    Auth pages (Sign out, loading and error states )

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useContext, useEffect, useState } from 'react' 
 import './App.css' 
 import Routing from './Router'
- 
+import { DataContext } from './Components/Context/DataProvider'
+import { auth } from './utils/firebase'
+import { Type } from './utils/action.type' 
 
 function App() { 
+  const [{ user }, dispatch] = useContext(DataContext);
+  useEffect(() => {
+    // check the auth state change from sign in to sign out vice versa and if the user signed in userInfo will be populated with the user data if not userInfo becomes null
+    auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch({ type: Type.SET_USER, user: authUser });
+      } else {
+        dispatch({ type: Type.SET_USER, user: null });
+      }
+    });
+  }, []);
   return (
     <>
         <Routing/>
